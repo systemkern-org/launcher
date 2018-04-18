5df37e67d0a094a19e706ba7fb14983bf9c6037b
Rainer Kern <rainer@systemkern.com>
2018-04-18 13:08:05 +0200

    Add module for default functionality

---
+++ default/src/main/kotlin/systemkern/PingController.kt
@@ -0,0 +1,22 @@
+package systemkern
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/default/ping")
+internal class PingController {
+
+    @GetMapping
+    fun ping(): PongDTO {
+        return PongDTO()
+    }
+
+}
+
+internal data class PongDTO(
+    val timestamp: LocalDateTime = LocalDateTime.now()
+)
+
