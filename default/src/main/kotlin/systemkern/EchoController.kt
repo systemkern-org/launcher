88119598e448089430ae2fc8ef0515bd1ef0730f
Rainer Kern <rainer@systemkern.com>
2018-04-19 15:33:24 +0200

    Add Echo Rest Controller

---
+++ default/src/main/kotlin/systemkern/EchoController.kt
@@ -0,0 +1,27 @@
+package systemkern
+
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/default/echo")
+internal class EchoController {
+
+    @PostMapping
+    fun echo(@Valid @RequestBody value: EchoDTO): EchoDTO {
+        return value
+    }
+
+}
+
+internal data class EchoDTO(
+    val id: Int,
+    val value: String,
+    val timestamp: LocalDateTime? = LocalDateTime.now()
+)
+
---
+++ default/src/main/kotlin/systemkern/JsonObjectMapper.kt
@@ -0,0 +1,29 @@
+package systemkern
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.stereotype.Component
+import org.zalando.jackson.datatype.money.MoneyModule
+
+@Component
+class JsonObjectMapper : ObjectMapper() {
+
+    init {
+        this.registerModule(KotlinModule())
+        // The java time module maps time as an array instead of as an object
+        this.registerModule(JavaTimeModule())
+        //Zalando money offers easier mapping for Money datatypes (CCY, Amount) than the standard mapping
+        this.registerModule(MoneyModule())
+        this.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        this.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true)
+        this.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+        this.configure(SerializationFeature.INDENT_OUTPUT, true)
+    }
+
+}
