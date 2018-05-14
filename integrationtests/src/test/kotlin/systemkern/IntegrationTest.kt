47be615ae46d821c7d1a26a0ba87c4699afa37da
rainer <rainer@systemkern.com>
2018-05-14 16:34:39 +0200

    Improve Integration Test configuration

---
+++ integrationtests/src/test/kotlin/systemkern/IntegrationTest.kt
@@ -0,0 +1,40 @@
+package systemkern
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.RuleChain
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.restdocs.JUnitRestDocumentation
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@RunWith(SpringRunner::class)
+@ActiveProfiles("integrationtest")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [CliEntryPoint::class])
+internal abstract class IntegrationTest {
+
+    val restDocumentation = JUnitRestDocumentation()
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+    @Autowired
+    lateinit var context: WebApplicationContext
+    lateinit var mockMvc: MockMvc
+    @get:Rule
+    val rules = RuleChain
+        .outerRule(restDocumentation)
+
+    @Before
+    fun setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+            .apply<DefaultMockMvcBuilder>(MockMvcRestDocumentation.documentationConfiguration(this.restDocumentation))
+            .build()
+    }
+}
