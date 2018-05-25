7b22165a2549139ebdbec65e0db3470409a2b4f8
AndresAusecha <daausecham@unal.edu.co>
2018-05-25 17:17:21 -0500

    #6 Password encryption added and working

--- user-profile/src/main/kotlin/systemkern/profile/UsersRepository.kt
+++ user-profile/src/main/kotlin/systemkern/profile/UsersRepository.kt
@@ -1,24 +1,29 @@
 package systemkern.profile
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.ImportResource
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Component
 
 @RepositoryRestResource
-internal interface UsersRepository : CrudRepository<User,Long>
+internal interface UsersRepository : CrudRepository<User, Long>
 
+@ImportResource("classpath:spring-security.xml")
 @Component
 @RepositoryEventHandler(User::class)
+
 internal class UserEventHandler(
-    /*@Autowired
-    internal val passwordEncoder: BCryptPasswordEncoder,*/
+    @Autowired
+    internal val passwordEncoder: BCryptPasswordEncoder,
     internal val userRepository: UsersRepository
 ) {
     @HandleBeforeCreate
     fun handleUserCreate(user: User) {
-        user.password = "123"//passwordEncoder.encode(user.password)
+        user.password = passwordEncoder.encode(user.password)
     }
 
 }
