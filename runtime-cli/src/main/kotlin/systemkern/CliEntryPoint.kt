554f26d9d84038be38c9eb77f6cf106d9b5f85c7
rainerkern <rainer@systemkern.com>
2018-06-12 09:59:16 +0200

    Improve Spring Boot configuration

--- runtime-cli/src/main/kotlin/systemkern/CliEntryPoint.kt
+++ runtime-cli/src/main/kotlin/systemkern/CliEntryPoint.kt
@@ -2,14 +2,18 @@ package systemkern
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan(basePackages = ["systemkern"])
-open class CliEntryPoint
+@EnableJpaRepositories(basePackages = ["systemkern"])
+@EntityScan(basePackages = ["systemkern"])
+class CliEntryPoint
 
 /**
  * Attention: Because of relative paths during app startup you might have to
