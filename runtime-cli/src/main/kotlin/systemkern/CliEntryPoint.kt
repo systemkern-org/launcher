4b2b514dbae099007c679cd410a2008a99350b74
AndresAusecha <daausecham@unal.edu.co>
2018-06-18 18:41:03 -0500

    #2 Trying to solve conflicts

--- runtime-cli/src/main/kotlin/systemkern/CliEntryPoint.kt
+++ runtime-cli/src/main/kotlin/systemkern/CliEntryPoint.kt
@@ -2,19 +2,23 @@ package systemkern
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan(basePackages = ["systemkern"])
+@EnableJpaRepositories(basePackages = ["systemkern"])
+@EntityScan(basePackages = ["systemkern"])
 class CliEntryPoint
 
 /**
  * Attention: Because of relative paths during app startup you might have to
  * set a working directory in your IDE:
- * For executing this class use: %CURRENT_MODULE%/target/classes as the working directory for your execution profile
+ * For executing this class use: main/target/classes as the working directory for your execution profile
  */
 fun main(args: Array<String>) {
     SpringApplication.run(CliEntryPoint::class.java, *args)
