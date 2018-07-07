efd8dffbb7a979de3db96c8b718918cf4047f1e9
AndresAusecha <daausecham@unal.edu.co>
2018-07-07 13:31:52 -0500

    #25 Updated auth service

--- user-profile/src/main/kotlin/systemkern/profile/AuthenticationService.kt
+++ user-profile/src/main/kotlin/systemkern/profile/AuthenticationService.kt
@@ -1,22 +1,26 @@
 package systemkern.profile
 
+import org.springframework.security.core.Authentication
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 import java.util.*
 import javax.servlet.http.HttpServletRequest
 import kotlin.collections.HashMap
+import java.time.Duration
 
 @Service
-internal class AuthenticationService(
-    val repo: UserProfileRepository
-) {
-    private var inactiveInterval:Long = 0
-    private var maxInactiveIntervalMilis = 0
+internal class AuthenticationService(val repo: UserProfileRepository,
+                                     val passwordEncoder: BCryptPasswordEncoder,
+                                     val sessionTimeOut: Duration) {
     val tokens: HashMap<UUID, AuthenticationResponse> = HashMap()
 
-    internal fun findByUsername(username: String)= repo.findByUsername(username)
+    internal fun findByUsername(username: String) =
+        repo.findByUsername(username)
+
     internal fun isValidToken(token: UUID, request: HttpServletRequest): Boolean {
-        inactiveInterval = System.currentTimeMillis() - request.session.lastAccessedTime
-        maxInactiveIntervalMilis = request.session.maxInactiveInterval * 1000
+        val inactiveInterval = System.currentTimeMillis() - request.session.lastAccessedTime
+        val maxInactiveIntervalMilis = request.session.maxInactiveInterval * 1000
         if (tokens.containsKey(token)) {
             return inactiveInterval <= maxInactiveIntervalMilis
         }
@@ -30,4 +34,26 @@ internal class AuthenticationService(
     internal fun deleteToken(token: UUID) {
         tokens.remove(token)
     }
+
+    @Throws(UserNotFoundException::class)
+    internal fun authenticationProcess(auth: Authentication,
+                                       password: String
+    ): AuthenticationResponse {
+        val user = findByUsername(auth.principal.toString())
+        val emailVerification = user.emailVerificationList.last()
+        if (!passwordEncoder.matches(password, user.password)
+            && emailVerification.completionDate <= emailVerification.creationDate)
+            throw UserNotFoundException("UserNotFoundException")
+        val token: UUID = UUID.fromString(auth.credentials.toString())
+        val validUntil = LocalDateTime.now().plusMinutes(sessionTimeOut.toMinutes())
+        val authResp = AuthenticationResponse(
+            token = token,
+            username = user.username,
+            userId = user.id_userProfile,
+            validUntil = validUntil
+        )
+        saveToken(token, authResp)
+        return authResp
+    }
 }
+
