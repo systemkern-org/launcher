4ca817f707b31765ac9de2ff65bce28c669ea970
AndresAusecha <daausecham@unal.edu.co>
2018-07-06 20:08:42 -0500

    #24 Formated code

--- user-profile/src/main/kotlin/systemkern/profile/MailUtility.kt
+++ user-profile/src/main/kotlin/systemkern/profile/MailUtility.kt
@@ -14,25 +14,24 @@ internal class MailUtility(@Autowired
     internal var urlToVerify: String = ""
     internal val message: SimpleMailMessage = SimpleMailMessage()
 
-    internal fun createEmailMessage(emailAddress: String,
-                                    tokenId: UUID,
-                                    baseUrl: String,
-                                    subject: String
-    ){
+    internal fun createEmailMessage(
+        emailAddress: String,
+        tokenId: UUID,
+        baseUrl: String,
+        subject: String
+    ) {
         message.setTo(emailAddress)
         message.subject = subject
-        urlToVerify = buildLink(tokenId,baseUrl)
+        urlToVerify = buildLink(tokenId, baseUrl)
         message.text = urlToVerify
     }
 
-    private fun buildLink(tokenId: UUID,baseUrl: String): String {
-
+    private fun buildLink(tokenId: UUID, baseUrl: String): String {
         var url = "http://"
         url += InetAddress.getLocalHost().hostAddress
         url += ":8080"
         url += baseUrl + tokenId.toString()
         return url
-
     }
 
     internal fun sendMessage() =
