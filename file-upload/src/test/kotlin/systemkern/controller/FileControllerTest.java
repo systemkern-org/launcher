af8856816f9472d0e1158aefe82692d4a6515e86
Juan López Villarán <juanlv989@gmail.com>
2018-08-05 02:22:23 +0200

    Update FileControllerTest.java

--- file-upload/src/test/kotlin/systemkern/controller/FileControllerTest.java
+++ file-upload/src/test/kotlin/systemkern/controller/FileControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import systemkern.configuration.FileStorageProperties;
 import systemkern.service.FileStorageService;
 
