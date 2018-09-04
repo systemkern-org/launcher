68f1d47c725a7bcc77241591563459adec2da07e
AndresAusecha <daausecham@unal.edu.co>
2018-09-04 06:49:07 -0500

    Provided an implementation of NN and text processor, looking for complements to smile

--- hermes/src/main/kotlin/DataHandler.kt
+++ hermes/src/main/kotlin/DataHandler.kt
@@ -2,6 +2,8 @@ package systemkern
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import smile.regression.NeuralNetwork
+import systemkern.hermes.NeuralNetworkImplementation
 import systemkern.utils.Utils
 import java.io.File
 import java.io.InputStream
@@ -9,102 +11,136 @@ import java.util.ArrayList
 import java.util.Collections.sort
 
 class DataHandler(
-    private val gson : Gson = Gson(),
+    private val googleJson : Gson = Gson(),
     private var classes : MutableList<String> = mutableListOf(),
-    private val documents : MutableList<MutableMap<ArrayList<String>?,String>> = mutableListOf()){
+    private val documents : MutableList<MutableMap<ArrayList<String>?,String>> = mutableListOf(),
+    private var bags: Array<DoubleArray> = arrayOf(),
+    private var outPutRows: Array<DoubleArray> = arrayOf(),
+    private var words : ArrayList<String> = arrayListOf(),
+    private var utils : Utils = Utils()) {
 
-    private var words : MutableList<String>? = null
-    private var utils : Utils? = null
 
-    init{
-        utils = Utils()
-    }
+    //TODO: the next element will be moved when tests are finished
+    private var nnImpl : NeuralNetworkImplementation? = null
+    //TODO:-------------------------------------------------------
 
-    fun loadJsonFile(pathFileToFile: String) : List<Intent>{
-        val inputStream: InputStream = File(pathFileToFile).inputStream()
-        val dataJson = inputStream.bufferedReader().use { it.readText() }
-        return gson.fromJson(dataJson, object : TypeToken<List<Intent>>() {}.type)
-    }
+    var bagLength = 0
 
-    private fun tokenizePhrases(phrase : String) : ArrayList<String>? {
-       return utils?.tokenize(phrase)
-    }
+    // To load file where structured text is located
+        fun loadJsonFile(pathFileToFile: String) : List<Intent>{
+            val inputStream: InputStream = File(pathFileToFile).inputStream()
+            val dataJson = inputStream.bufferedReader().use { it.readText() }
+            return googleJson.fromJson(dataJson, object : TypeToken<List<Intent>>() {}.type)
+        }
 
-    fun tokenizeWordsInIntents(intents : List<Intent>){
-        words = mutableListOf()
-        var tokens : ArrayList<String>?
-        for(intent in intents){
-            for (pattern in intent.patterns){
-                tokens = tokenizePhrases(cleanText(pattern))
-                if (tokens != null) {
-                    documents.add(mutableMapOf(Pair(tokens,intent.tag)))
-                    for(token in tokens){
-                        if (!(words?.contains(token) as Boolean)){
-                            words?.add(token)
-                        }
-                        if(!classes.contains(intent.tag)){
-                            classes.add(intent.tag)
+        //To create arrays of words from phrases,spaces and tokens of language are deleted
+        private fun tokenizePhrases(phrase : String) : ArrayList<String>? {
+           return utils.tokenize(cleanText(phrase))
+        }
+
+        // To create arrays of documents,words and classes, classes are useful to classify words into categories,
+        // documents gather tokens(words in sentences) and classes.
+        fun tokenizeWordsInIntents(intents : List<Intent>){
+            var tokens : ArrayList<String>?
+            for(intent in intents){
+                for (pattern in intent.patterns){
+                    tokens = tokenizePhrases(pattern)
+                    if (tokens != null) {
+                        documents.add(mutableMapOf(Pair(tokens,intent.tag)))
+                        for(token in tokens){
+                            if (!(words.contains(token))){
+                                words.add(token)
+                            }
+                            if(!classes.contains(intent.tag)){
+                                classes.add(intent.tag)
+                            }
                         }
                     }
                 }
             }
+            sort(words)
+            createArrayOfZerosAndOnes()
         }
-        sort(words)
-        createArrayOfZerosAndOnes(words)
-    }
 
-    private fun createArrayOfZerosAndOnes(words : MutableList<String>?){
 
-        for(doc in documents){
-            var bag = arrayListOf<Int>()
-            val patterWords = doc[doc.keys.first() ]
-            if (words != null){
-                for (word in words){
-                    if(patterWords?.contains(word) as Boolean){
-                        bag.add(1)
-                    }else {
-                        bag.add(0)
+        private fun createArrayOfZerosAndOnes(){
+            var bag : DoubleArray = doubleArrayOf()
+            for(doc in documents){
+                val patterWords = doc[doc.keys.first()]
+                    for (word in words){
+                        if(patterWords?.contains(word) as Boolean){
+                            bag = bag.plus(1.0)
+                        }else {
+                            bag = bag.plus(0.0)
+                        }
+                    }
+                bags = bags.plus(bag)
+                val outputRow = createArrayListOfZeros(classes.size)
+                outputRow[classes.indexOf(doc[doc.keys.first()])] = 1.0
+                outPutRows = outPutRows.plus(outputRow)
+                bag = doubleArrayOf()
+            }
+            bagLength = bags[0].size
+            nnImpl = NeuralNetworkImplementation(NeuralNetwork.ActivationFunction.LOGISTIC_SIGMOID,words.size)
+            trainNeuralNetwork(bags,outPutRows)
+            val res = testsSentences()
+            print(res)
+        }
+
+        //Utility to create arrays filled out with zeros
+        private fun createArrayListOfZeros(arraySize : Int) : DoubleArray{
+            var listToFill = doubleArrayOf()
+            for (i in 0..(arraySize - 1)){
+                listToFill = listToFill.plus(0.0)
+            }
+            return listToFill
+        }
+
+        private fun trainNeuralNetwork(train_x : Array<DoubleArray>,train_y : Array<DoubleArray>){
+            for (y in train_y) {
+                nnImpl?.learn(arrayOf(train_x[train_y.indexOf(y)]),train_y[train_y.indexOf(y)])
+            }
+        }
+
+        private fun checkSentenceInDictionary(sentence : String) : DoubleArray {
+            val wordsInSentence = tokenizePhrases(sentence)
+            var zerosAndOnesArray = createArrayListOfZeros(bagLength)
+
+            if(wordsInSentence  !== null) {
+                for (word in wordsInSentence) {
+                    if(words.contains(word)){
+                        zerosAndOnesArray[words.indexOf(word)] = 1.0
                     }
                 }
             }
-            val outputRow = createArrayListOfZeros()
-            outputRow[classes.indexOf(doc[doc.keys.first()])] = 1
 
+            return zerosAndOnesArray
+        }
+
+        private fun testsSentences() : DoubleArray? {
+            val testSentence = "Hello how are you?"
+            return nnImpl?.predict(arrayOf(checkSentenceInDictionary(testSentence)))
+        }
+
+        // To delete contractions and useless tokens from phrases
+        private fun cleanText(text: String) : String {
+            var finalText = text
+            finalText = finalText.toLowerCase()
+            finalText = finalText.replace("i'm", "i am")
+            finalText = finalText.replace("\'s", " is")
+            finalText = finalText.replace("\'ll", " will")
+            finalText = finalText.replace("\'ve", " have")
+            finalText = finalText.replace("\'re", " are")
+            finalText = finalText.replace("\'d", " would")
+            finalText = finalText.replace("won't", "will not")
+            finalText = finalText.replace("can't", "cannot")
+            finalText = finalText.replace("don't", " do not")
+            finalText = finalText.replace("doesn't", " does not")
+            return finalText.replace("[-()\"#/@;:<>{}+=~|.?,]", "")
         }
     }
 
-    private fun createArrayListOfZeros() : MutableList<Int>{
-        val listToFill = mutableListOf<Int>()
-        for (i in 0..(classes.size - 1)){
-            listToFill.add(0)
-        }
-        return listToFill
-    }
-
-    private fun createAndTrainNeuralNetwork(){
-        //In this method i receive the arrays
-        //generated from files, then create and }
-        // train neural network.
-    }
-
-}
-
-private fun cleanText(text: String) : String {
-    var text = text
-    text = text.toLowerCase()
-    text = text.replace("i'm", "i am")
-    text = text.replace("\'s", " is")
-    text = text.replace("\'ll", " will")
-    text = text.replace("\'ve", " have")
-    text = text.replace("\'re", " are")
-    text = text.replace("\'d", " would")
-    text = text.replace("won't", "will not")
-    text = text.replace("can't", "cannot")
-    text = text.replace("don't", " do not")
-    text = text.replace("doesn't", " does not")
-    return text.replace("[-()\"#/@;:<>{}+=~|.?,]", "")
-}
-
+//Representation of json file structure
 data class Intent(
     val tag : String,
     val patterns : List<String>,
